a3754bec6efa7cd9f7db1480145dbab197919aa3
ticktakclock <ticchettio.di.un.orologio@gmail.com>
2020-04-14 20:58:02 +0900

    Add chart bar.

---
+++ src/components/ChartBar.vue
@@ -0,0 +1,21 @@
+<script>
+import { Bar, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Bar,
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+};
+</script>
